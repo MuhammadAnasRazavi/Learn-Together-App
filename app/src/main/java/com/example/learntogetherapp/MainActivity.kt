0d42084e91ea68037f1a4deadaad8fc0332a67c2
MuhammadAnasRazavi <muhammadanasrazavi@gmail.com>
2023-11-14 18:37:57 +0530

    Built a single page article which displays information.

--- app/src/main/java/com/example/learntogetherapp/MainActivity.kt
+++ app/src/main/java/com/example/learntogetherapp/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.learntogetherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogetherapp.ui.theme.LearnTogetherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DisplayArticle()
                 }
             }
         }
@@ -30,17 +40,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun DisplayArticle() {
+    ArticlePage(
+        image = painterResource(id = R.drawable.bg_compose_background),
+        title = stringResource(R.string.article_title),
+        shortDesc = stringResource(R.string.short_paragraph_text),
+        longDesc = stringResource(R.string.long_paragraph_text)
     )
 }
 
-@Preview(showBackground = true)
+@Composable
+fun ArticlePage(
+    image: Painter,
+    title: String,
+    shortDesc: String,
+    longDesc: String,
+    modifier: Modifier = Modifier
+) {
+
+    Column(modifier = modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = modifier.padding(16.dp)
+        )
+        Text(
+            text = shortDesc,
+            textAlign = TextAlign.Justify,
+            modifier = modifier.padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = longDesc,
+            textAlign = TextAlign.Justify,
+            modifier = modifier.padding(16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     LearnTogetherAppTheme {
-        Greeting("Android")
+        DisplayArticle()
     }
 }
